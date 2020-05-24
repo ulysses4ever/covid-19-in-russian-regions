94b8c6af513f0134f79aa68d0b49608063952f2f
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2020-05-23 22:25:55 -0400

    update get-covid script to check if the data have been updated

--- src/get-covid-minzdrav.sh
+++ src/get-covid-minzdrav.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-#
-# This is supposed to be run by Cron every day, because Minzdrav
-# doesn't preserve data for previous days
-#
-wget https://covid19.rosminzdrav.ru/wp-json/api/mapdata/ -O $(pwd)/../data/total-covid$(date +%m%d).json
+set -x
+base=$(pwd)/../data
+today=$(date +%m%d)
+out=${base}/covid${today}.json
+if [ -f ${out} ]
+then
+   exit
+fi
+tmp=${base}/tmp.json
+yest=$(date +%m%d --date="yesterday")
+last=${base}/covid${yest}.json
+wget https://covid19.rosminzdrav.ru/wp-json/api/mapdata/ -O ${tmp}
+df=$(diff ${last} ${tmp})
+if [ "$d" == "" ]
+then
+    rm ${tmp}
+else
+    mv ${tmp} ${out}
+fi
