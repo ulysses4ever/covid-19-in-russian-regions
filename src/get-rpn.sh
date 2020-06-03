ba95b8dba1e07c60744ba0359031f5cf77f7a7f7
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2020-06-03 23:21:04 +0200

    add data for June 3; more robust get-rpn script

--- src/get-rpn.sh
+++ src/get-rpn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -x
+set -xe
+
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 base="${mydir}/../data/rpn-url-ids"
 today="$(date +%m%d)"
@@ -9,17 +10,27 @@ then
    exit
 fi
 
+# Get most recent URL IDs
 wget https://www.rospotrebnadzor.ru/about/info/news/
 iconv -f CP1251 -t UTF-8 index.html -o index.utf8.html
 urlid1=$(grep "О подтвержденных случаях новой коронавирусной инфекции COVID-2019 в России" index.utf8.html | head -1 | grep -Po "[0-9]{5}")
 urlid2=$(grep "Информационный бюллетень о ситуации и принимаемых мерах по недопущению распространения заболеваний, вызванных новым коронавирусом" index.utf8.html | head -1 | grep -Po "[0-9]{5}")
 rm -f index.html index.utf8.html
-
-new="(${urlid1}, ${urlid2})"
+if [ "${urlid1}" == "" ] && [ "${urlid2}" == "" ]
+then
+    exit
+fi
 yest="$(date +%m%d --date="yesterday")"
 last="${base}/${yest}"
-df=$(echo ${new} | diff - "${last}" | head -1)
-if [ "${urlid1}" != "" ] && [ "${urlid2}" != "" ] && [ "${df}" != ""   ]
+
+# If we've alaredy seen these URL IDs, exit: the data is not up yet
+if grep -q "${urlid1}" "${last}"
 then
-    echo ${new} > "${out}"
+    exit
 fi
+if grep -q "${urlid2}" "${last}"
+then
+    exit
+fi
+
+echo "(${urlid1}, ${urlid2})" > "${out}"
